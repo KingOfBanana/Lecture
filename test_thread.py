d797ad72e3a15f2a7afff9319d3bab29f5e0d106
KingOfBanana <82736124@qq.com>
2016-12-01 22:28:30 +0800

    try to use multithreads to get lecture

---
+++ test_thread.py
@@ -0,0 +1,41 @@
+import time, threading
+
+from lecture import *
+import random
+
+chairId = '654'
+flag = 0
+lock = threading.Lock()
+result = 'init'
+total_thread_num = 4
+def run_thread(chairId):
+	global flag
+	global result
+	
+	lecture = XMU_Lecture()
+	while flag == 0:
+		state = lecture.getLectureState(chairId)
+		if state == 2 or state == -1:
+			lock.acquire()
+			try:
+				flag = 1
+			finally:
+				lock.release()
+				if state == 2:
+					print('Success!')
+				elif state == -1:
+					print('Failed!')
+		elif state == 0:
+			print('Please wait!')
+		else:
+			try:
+				flag = 1
+			finally:
+				lock.release()
+				result = lecture.getLecture(chairId, state)
+				print(result)
+
+for i in range(total_thread_num):
+	t = threading.Thread(target=run_thread, args=(chairId, ))
+	t.start()
+	time.sleep(0.8)
